90dab27e935f83133eb186942caffd0831f206b5
Samriddhi Sharma_AC <sharmasamriddhi871@gmail.com>
2026-09-14 00:52:08 +0530

    ArraySum.java

--- Assignments/oopj/day1_assignment/Arrays_EasyAssignment_java/ArraySum.java
+++ Assignments/oopj/day1_assignment/Arrays_EasyAssignment_java/ArraySum.java
@@ -5,8 +5,8 @@ public class ArraySum {
     public static void main(String[] args) {
         int[] arr = {5, 2, 8, 1, 3};
         int sum = 0;
-        for(int i = 0; i < arr.length; i++) {
-            sum = sum + arr[i];
+        for(int tmp = 0; tmp < arr.length; tmp++) {
+            sum = sum + arr[tmp];
         }
         System.out.println("Sum = " + sum);
     }
