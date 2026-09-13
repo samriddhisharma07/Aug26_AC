5544dc4d94f792189eb637529864202c12477ffe
Samriddhi Sharma_AC <sharmasamriddhi871@gmail.com>
2026-09-14 00:51:10 +0530

    ArraySort.java

--- Assignments/oopj/day1_assignment/Arrays_EasyAssignment_java/ArraySort.java
+++ Assignments/oopj/day1_assignment/Arrays_EasyAssignment_java/ArraySort.java
@@ -4,17 +4,17 @@ public class ArraySort {
 
     public static void main(String[] args) {
         int[] arr = {5, 2, 8, 1, 3};
-        for(int i = 0; i < arr.length; i++) {
-            for(int j = i + 1; j < arr.length; j++) {
-                if(arr[i] > arr[j]) {
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
+        for(int tmp = 0; tmp < arr.length; tmp++) {
+            for(int jTmp = tmp + 1; jTmp < arr.length; jTmp++) {
+                if(arr[tmp] > arr[jTmp]) {
+                    int temp = arr[tmp];
+                    arr[tmp] = arr[jTmp];
+                    arr[jTmp] = temp;
                 }
             }
         }
-        for(int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
+        for(int tmp = 0; tmp < arr.length; tmp++) {
+            System.out.print(arr[tmp] + " ");
         }
     }
 }
